f197378fe2c6491a175d6b660213e7df8d604e91
goncas200 <111070886+goncas200@users.noreply.github.com>
2023-05-20 18:59:35 +0100

    Release

--- CalculatorC++.cpp
+++ CalculatorC++.cpp
@@ -1,58 +1,91 @@
 #include <iostream>
+#include <string>
 #include <cmath>
 
 using namespace std;
 
+double better_double(string s) {
+    string final;
+    for (char c : s) {
+        if (c == ',')
+            final += '.';
+
+        else if (c != '1' and c != '2' and c != '3' and c != '4' and c != '5' and c != '6' and c != '7' and c != '8' and c != '9' and c != '0')
+            return 1.0;
+
+        else
+            final += c;
+    }
+    return stod(final);
+}
+
 void add() {
-    double first, second;
+    string first, second;
+    double dfirst, dsecond;
     cout << "First number: ";
     cin >> first;
     cout << "Second number: ";
     cin >> second;
-    cout << first + second << "\n";
+    dfirst = better_double(first);
+    dsecond = better_double(second);
+    cout << dfirst + dsecond << "\n";
 }
 
 void subtract() {
-    double first, second;
+    string first, second;
+    double dfirst, dsecond;
     cout << "First number: ";
     cin >> first;
     cout << "Second number: ";
     cin >> second;
-    cout <<  first - second << "\n";
+    dfirst = better_double(first);
+    dsecond = better_double(second);
+    cout << dfirst - dsecond << "\n";
 }
 
 void multiply() {
-    double first, second;
+    string first, second;
+    double dfirst, dsecond;
     cout << "First number: ";
     cin >> first;
     cout << "Second number: ";
     cin >> second;
-    cout <<  first * second << "\n";
+    dfirst = better_double(first);
+    dsecond = better_double(second);
+    cout << dfirst * dsecond << "\n";
 }
 
 void divide() {
-    double first, second;
+    string first, second;
+    double dfirst, dsecond;
     cout << "First number: ";
     cin >> first;
     cout << "Second number: ";
     cin >> second;
-    cout << first / second << "\n";
+    dfirst = better_double(first);
+    dsecond = better_double(second);
+    cout << dfirst / dsecond << "\n";
 }
 
 void power() {
-    double first, second;
+    string first, second;
+    double dfirst, dsecond;
     cout << "First number: ";
     cin >> first;
     cout << "Second number: ";
     cin >> second;
-    cout << pow(first, second) << "\n";
+    dfirst = better_double(first);
+    dsecond = better_double(second);
+    cout << pow(dfirst, dsecond) << "\n";
 }
 
 void sqrrt() {
-    double first;
+    string first;
+    double dfirst;
     cout << "Number: ";
     cin >> first;
-    cout << sqrt(first) << "\n";
+    dfirst = better_double(first);
+    cout << sqrt(dfirst) << "\n";
 }
 
 int main()
