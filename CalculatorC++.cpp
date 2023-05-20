1eee87f5f85289f6fe309b14f168640f44d4532e
goncas200 <111070886+goncas200@users.noreply.github.com>
2023-05-20 17:52:23 +0100

    Probably realease

--- CalculatorC++.cpp
+++ CalculatorC++.cpp
@@ -1,9 +1,101 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+const int MAX_LENGTH = 64;
+
+void add() {
+    double first, second;
+    cout << "First number: ";
+    cin >> first;
+    cout << "Second number: ";
+    cin >> second;
+    cout << first + second << "\n";
+}
+
+void subtract() {
+    double first, second;
+    cout << "First number: ";
+    cin >> first;
+    cout << "Second number: ";
+    cin >> second;
+    cout <<  first - second << "\n";
+}
+
+void multiply() {
+    double first, second;
+    cout << "First number: ";
+    cin >> first;
+    cout << "Second number: ";
+    cin >> second;
+    cout <<  first * second << "\n";
+}
+
+void divide() {
+    double first, second;
+    cout << "First number: ";
+    cin >> first;
+    cout << "Second number: ";
+    cin >> second;
+    cout << first / second << "\n";
+}
+
+void power() {
+    double first, second;
+    cout << "First number: ";
+    cin >> first;
+    cout << "Second number: ";
+    cin >> second;
+    cout << pow(first, second) << "\n";
+}
+
+void sqrrt() {
+    double first;
+    cout << "Number: ";
+    cin >> first;
+    cout << sqrt(first) << "\n";
+}
+
 int main()
 {
-    cout << "Hello World!\n";
-    return 0;
+    cout << "Welcome To the calculator!!\n";
+    while (true) {
+        string user;
+        cout << "What operation do you want to do? [+ (add); - (subtract); * (multiply); / (divide); ** (power); % (squareroot); exit (to exit the program)]: ";
+        cin >> user;
+        try {
+            if (user == "+")
+                add();
+
+            else if (user == "-")
+                subtract();
+
+            else if (user == "*")
+                multiply();
+
+            else if (user == "/")
+                divide();
+
+            else if (user == "**")
+                power();
+
+            else if (user == "%")
+                sqrrt();
+
+            else if (user == "exit")
+                return 0;
+
+            else
+                throw "Error: Invalid operation\n";
+
+        }
+        catch (const char s[]) {
+            cout << s;
+        }
+        catch (...) {
+            cout << "Error: Try to put . in floats instead of ,\n";
+        }
+    }
+    return 1;
 }
