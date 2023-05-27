73d32b6ed9ea714a0d809bc71a57220141c50388
goncas200 <111070886+goncas200@users.noreply.github.com>
2023-05-28 00:31:58 +0100

    Full change on CalculatorCpp

--- CalculatorC++.cpp
+++ CalculatorC++.cpp
@@ -1,130 +1,136 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <vector>
 using namespace std;
 
-double better_double(string s) {
-    string final;
-    for (char c : s) {
-        if (c == ',')
-            final += '.';
-
-        else if (c != '1' and c != '2' and c != '3' and c != '4' and c != '5' and c != '6' and c != '7' and c != '8' and c != '9' and c != '0')
-            return 1.0;
-
-        else
-            final += c;
-    }
-    return stod(final);
+double add(double a, double b) {
+    return a + b;
 }
 
-void add() {
-    string first, second;
-    double dfirst, dsecond;
-    cout << "First number: ";
-    cin >> first;
-    cout << "Second number: ";
-    cin >> second;
-    dfirst = better_double(first);
-    dsecond = better_double(second);
-    cout << dfirst + dsecond << "\n";
+double subtract(double a, double b) {
+    return a - b;
 }
 
-void subtract() {
-    string first, second;
-    double dfirst, dsecond;
-    cout << "First number: ";
-    cin >> first;
-    cout << "Second number: ";
-    cin >> second;
-    dfirst = better_double(first);
-    dsecond = better_double(second);
-    cout << dfirst - dsecond << "\n";
+double multiply(double a, double b) {
+    return a * b;
 }
 
-void multiply() {
-    string first, second;
-    double dfirst, dsecond;
-    cout << "First number: ";
-    cin >> first;
-    cout << "Second number: ";
-    cin >> second;
-    dfirst = better_double(first);
-    dsecond = better_double(second);
-    cout << dfirst * dsecond << "\n";
+double divide(double a, double b) {
+    return a / b;
 }
 
-void divide() {
-    string first, second;
-    double dfirst, dsecond;
-    cout << "First number: ";
-    cin >> first;
-    cout << "Second number: ";
-    cin >> second;
-    dfirst = better_double(first);
-    dsecond = better_double(second);
-    cout << dfirst / dsecond << "\n";
-}
-
-void power() {
-    string first, second;
-    double dfirst, dsecond;
-    cout << "First number: ";
-    cin >> first;
-    cout << "Second number: ";
-    cin >> second;
-    dfirst = better_double(first);
-    dsecond = better_double(second);
-    cout << pow(dfirst, dsecond) << "\n";
-}
-
-void sqrrt() {
-    string first;
-    double dfirst;
-    cout << "Number: ";
-    cin >> first;
-    dfirst = better_double(first);
-    cout << sqrt(dfirst) << "\n";
+double power(double a, double b) {
+    return pow(a, b);
 }
 
 int main() {
-    cout << "Welcome To the calculator!!\n";
+    cout << "Welcome To the calculator!!\n\n";
     while (true) {
         string user;
-        cout << "What operation do you want to do? [+ (add); - (subtract); * (multiply); / (divide); ** (power); % (squareroot); exit (to exit the program)]: ";
-        cin >> user;
-        try {
-            if (user == "+")
-                add();
-
-            else if (user == "-")
-                subtract();
-
-            else if (user == "*")
-                multiply();
-
-            else if (user == "/")
-                divide();
-
-            else if (user == "**")
-                power();
-
-            else if (user == "%")
-                sqrrt();
-
-            else if (user == "exit")
-                return 0;
-
-            else
-                throw "Error: Invalid operation\n";
-
+        string s;
+        vector<double> number;
+        vector<char> operations;
+        bool first = true;
+        int number_interator = 0;
+        double ffinal = 0;
+        cout << "Operation: ";
+        getline(cin, user);
+        for (char i : user) {
+            if (i == ' ') {
+                continue;
+            }
+            if (i == '+') {
+                number.push_back(stod(s));
+                operations.push_back(i);
+                s = "";
+            }
+            else if (i == '-') {
+                number.push_back(stod(s));
+                operations.push_back(i);
+                s = "";
+            }
+            else if (i == '*') {
+                number.push_back(stod(s));
+                operations.push_back(i);
+                s = "";
+            }
+            else if (i == '/') {
+                number.push_back(stod(s));
+                operations.push_back(i);
+                s = "";
+            }
+            else if (i == '%') {
+                number.push_back(stod(s));
+                operations.push_back(i);
+                s = "";
+            }
+            else if (i == '%') {
+                number.push_back(stod(s));
+                operations.push_back(i);
+                s = "";
+            }
+            else s += i;
         }
-        catch (const char s[]) {
-            cout << s;
+        number.push_back(stod(s));
+        for (char& i : operations) {
+            if (i == '+') {
+                if (first) {
+                    ffinal = add(number[number_interator], number[number_interator + 1]);
+                    number_interator += 2;
+                    first = false;
+                }
+                else {
+                    ffinal = add(ffinal, number[number_interator]);
+                    ++number_interator;
+                }
+            }
+            else if (i == '-') {
+                if (first) {
+                    ffinal = subtract(number[number_interator], number[number_interator + 1]);
+                    number_interator += 2;
+                    first = false;
+                }
+                else {
+                    ffinal = subtract(ffinal, number[number_interator]);
+                    ++number_interator;
+                }
+            }
+            else if (i == '*') {
+                if (first) {
+                    ffinal = multiply(number[number_interator], number[number_interator + 1]);
+                    number_interator += 2;
+                    first = false;
+                }
+                else {
+                    ffinal = multiply(ffinal, number[number_interator]);
+                    ++number_interator;
+                }
+            }
+            else if (i == '/') {
+                if (first) {
+                    ffinal = divide(number[number_interator], number[number_interator + 1]);
+                    number_interator += 2;
+                    first = false;
+                }
+                else {
+                    ffinal = divide(ffinal, number[number_interator]);
+                    ++number_interator;
+                }
+            }
+            else if (i == '%') {
+                if (first) {
+                    ffinal = power(number[number_interator], number[number_interator + 1]);
+                    number_interator += 2;
+                    first = false;
+                }
+                else {
+                    ffinal = power(ffinal, number[number_interator]);
+                    ++number_interator;
+                }
+            }
         }
-        catch (...) {
-            cout << "Error: Try to put . in floats instead of ,\n";
-        }
+        cout << ffinal << "\n";
     }
     return 1;
 }
